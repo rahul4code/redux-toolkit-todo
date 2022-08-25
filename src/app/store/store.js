cbef61e5e1997b5e76c795cea2b35be6dd648c3e
rahul4code <you@example.com>
2022-08-25 15:47:29 +0530

    combineResucers

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlices from "../slices/counterSlices";
 import userInfoSlices from '../slices/userInfoSlices'
+import { combineReducers } from "@reduxjs/toolkit";
+
+const rootReducer=combineReducers({counter:counterSlices,
+    userInfo:userInfoSlices
+    })
 
 export const store=configureStore({
-    reducer:{counter:counterSlices,
-    userInfo:userInfoSlices
-    }
+    reducer: rootReducer
 })
